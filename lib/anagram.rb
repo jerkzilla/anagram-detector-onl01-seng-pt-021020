dc1075a85390bcf6c35a539fb45e5510e50caa34
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-05 00:08:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,8 +5,9 @@ def initialize(word)
 end
 
 def match(array_of_words)
-  array_of_words.select do |element| (@word.split("").sort) == (element.split("").sort)
+  array_of_words.select do |element| (@word.split("")) == (element.split(""))
 end
+
 end
 
 
